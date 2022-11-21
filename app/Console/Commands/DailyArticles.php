04cad886fdb54e40c1cf41e45050012965dd5d73
Ana Thais Senger <anathaissenger@hotmail.com>
2022-11-21 08:57:55 -0300

    arrumando cron

--- app/Console/Commands/DailyArticles.php
+++ app/Console/Commands/DailyArticles.php
@@ -34,12 +34,12 @@ class DailyArticles extends Command
             foreach ($articles as $article){
         
              DB::connection('mysql')->insert('insert into articles (id, featured, title, url, imageUrl, newsSite, summary, publishedAt, created_at) values (?,?,?,?,?,?,?,?,?)',  array($article->id , $article->featured , $article->title,   $article->url, $article->imageUrl,  $article->newsSite, $article->summary, $article->publishedAt, $article->created_at));
-        
-             DB::table('new_articles')->delete();
-        
             }
+            DB::table('new_articles')->delete();
 
         $this->info('New articles have been added.');
+        }else{
+        $this->info('No articles have been added today.');
     }
 }
 }
